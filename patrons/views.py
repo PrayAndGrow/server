b8d900d56a2cff89eb7bfe535c48548117c8f64a
Michał Garapich <michal@garapich.pl>
2016-04-09 22:24:28 +0200

    patronsearch api

--- patrons/views.py
+++ patrons/views.py
@@ -1,4 +1,10 @@
+import datetime
+
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Patron
 from .serializers import PatronSerializer
 
@@ -9,3 +15,14 @@ class PatronSet(viewsets.ModelViewSet):
     """
     queryset = Patron.objects.all()
     serializer_class = PatronSerializer
+
+
+@api_view(['GET', 'POST'])
+def patron_search(request):
+    """
+    Search for patrons in the neighbourhood and for today.
+    """
+    today = datetime.date.today()
+    patrons = Patron.objects.filter(Q(reminiscence=today) | Q(canonisation=today))
+    serializer = PatronSerializer(patrons, many=True)
+    return Response({'response': serializer.data})
