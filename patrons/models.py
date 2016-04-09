385fc122ba7346be6c6b9320de7db0cc81930185
Michał Garapich <michal@garapich.pl>
2016-04-09 15:56:50 +0200

    Added patrons app

---
+++ patrons/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Place(models.Model):
+    """
+    Representation of a place that could notice an activity
+    of one saint.
+    """
+    name = models.CharField(max_length=1024)
+
+
+class Activity(models.Model):
+    """
+    An activity that can have a patron attached.
+    """
+    name = models.CharField(max_length=1024)
+
+
+class Patron(models.Model):
+    """
+    A model representing one saint
+    """
+    name = models.CharField(max_length=1024)
+    url = models.URLField()
+    reminiscence = models.DateField(blank=True)
+    canonisation = models.DateField(blank=True)
+    places = models.ManyToManyField(Place, blank=True)
+    activities = models.ManyToManyField(Activity, blank=True)
