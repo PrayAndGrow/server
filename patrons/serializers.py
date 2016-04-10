eb7d8a507c73e1bc8e730a3f6c09c77f7d311709
Michał Garapich <michal@garapich.pl>
2016-04-10 03:31:22 +0200

    Moar serializers

--- patrons/serializers.py
+++ patrons/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import Patron
+from .models import Patron, Prayer, Place
+
+
+class PrayerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Prayer
+        fields = ('prayer',)
+
+
+class PlaceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Place
+        fields = ('name',)
 
 
 class PatronSerializer(serializers.HyperlinkedModelSerializer):
+    prayer = PrayerSerializer()
+    places = PlaceSerializer(many=True)
+
     class Meta:
         model = Patron
-        fields = ('name', 'url', 'reminiscence', 'canonisation')
+        fields = ('name', 'url', 'reminiscence', 'canonisation', 'prayer', 'places')
--- patrons/views.py
+++ patrons/views.py
@@ -9,13 +9,13 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Patron
+from .models import Patron, Prayer
 from .serializers import PatronSerializer
 
 
 class PatronSet(viewsets.ModelViewSet):
     """
-    API endpoint for Patrons
+    API endpoint for Patrons.
     """
     queryset = Patron.objects.all()
     serializer_class = PatronSerializer
