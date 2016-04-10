eda1ea98a121a5ce8e0abae06604e0d8e71f9425
Michał Garapich <michal@garapich.pl>
2016-04-10 02:29:23 +0200

    Places API for patrons

--- patrons/views.py
+++ patrons/views.py
@@ -1,6 +1,10 @@
 import datetime
+from functools import reduce
+
+import requests
 
 from django.db.models import Q
+from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -17,12 +21,37 @@ class PatronSet(viewsets.ModelViewSet):
     serializer_class = PatronSerializer
 
 
+def get_places(lat, lon):
+    endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    radius = '100'
+
+    url = '{0}?key={1}&location={2},{3}&radius={4}'.format(endpoint, settings.GOOGLE_PLACES_API_KEY, lat, lon, radius)
+    r = requests.get(url)
+    names = []
+    try:
+        json = r.json()
+        for entry in json["results"]:
+            names.append(entry["name"])
+
+        return names
+    except:
+        return []
+
+
 @api_view(['GET', 'POST'])
 def patron_search(request):
     """
     Search for patrons in the neighbourhood and for today.
     """
     today = datetime.date.today()
-    patrons = Patron.objects.filter(Q(reminiscence=today) | Q(canonisation=today))
+    if 'latitude' and 'longitude' in request.data:
+        places = get_places(requests.data['latitude'], request.data['longitude'])
+
+    try:
+        patrons = Patron.objects.filter(Q(reminiscence=today) |
+            reduce(lambda x, y: x | y, [Q(places__name__icontains=place) for place in places]))
+    except UnboundLocalError:
+        patrons = Patron.objects.filter(Q(reminiscence=today) | Q(canonisation=today))
+
     serializer = PatronSerializer(patrons, many=True)
     return Response({'response': serializer.data})
